e05378b6a0a23bb2520dfdac0aa8039d62d6db6a
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-09-21 09:54:18 +1000

    Buildkite: Do --no-nix-pure setting through stack.yaml

--- .buildkite/rebuild.hs
+++ .buildkite/rebuild.hs
@@ -156,7 +156,6 @@ buildStep dryRun bk nightly = do
         run dryRun "stack" $ concat
             [ color "always"
             , [ "build" ]
-            , [ "--no-nix-pure" ]
             , [ "--bench" ]
             , [ "--no-run-benchmarks" ]
             , [ "--haddock" ]
@@ -170,7 +169,6 @@ buildStep dryRun bk nightly = do
         run dryRun "stack" $ concat
             [ color "always"
             , [ "test" ]
-            , [ "--no-nix-pure" ]
             , fast opt
             , case qaLevel nightly bk of
                 QuickTest -> skip "integration" <> skip "jormungandr-integration"
